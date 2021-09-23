02530d333181ba48f0d64617b9c4a05c8c3cb0d2
art-savinci <lowdeep1004@gamail.com>
2021-09-23 17:37:57 +0900

    test

---
+++ js/music-list.js
@@ -0,0 +1,196 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Snowman",  
+    artist: "Sia",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Summer Days", 
+    artist: "Martin Garrix",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Nappy roots",
+    artist: "Good Day",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Gimme Gimme",
+    artist: "Johnny Stimson",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Aubrey", 
+    artist: "BREAD",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Ho Ho Ho",
+    artist: "Sia",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "I Am Better Off", 
+    artist: "Wildson",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Let It Be Me",  
+    artist: "Phil Everly",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Warm On A Cold Night", 
+    artist: "HONNE",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Thunderclouds", 
+    artist: "Sia",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "If", 
+    artist: "Sissel",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Cheap Thrills", 
+    artist: "Sia",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "CandyCaneLane", 
+    artist: "Sia",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "The Greatest", 
+    artist: "Sia",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Stand by Me", 
+    artist: "Alexandra",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Wishes", 
+    artist: "Emi Fujita",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Over The Rainbow", 
+    artist: "Debison",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Fall in love sometimes", 
+    artist: "Elton John",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Count On Me", 
+    artist: "Jefferson Starship",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "You are Only Lonely", 
+    artist: "J.D. Souther",
+    img: "music-20",
+    src: "music-20"
+  }, 
+  {
+    name: "When we disco", 
+    artist: "J.Y.Park",
+    img: "music-21",
+    src: "music-21"
+  }, 
+  {
+    name: "Love Yourself", 
+    artist: "Justin Bieber",
+    img: "music-22",
+    src: "music-22"
+  }, 
+  {
+    name: "Peaches", 
+    artist: "Justin Bieber",
+    img: "music-23",
+    src: "music-23"
+  }, 
+  {
+    name: "Never Stop", 
+    artist: "Chilly Gonzales",
+    img: "music-24",
+    src: "music-24"
+  }, 
+  {
+    name: "The water is wide", 
+    artist: "Karla Bonoff",
+    img: "music-25",
+    src: "music-25"
+  }, 
+  {
+    name: "Four strong winds", 
+    artist: "Neil Young",
+    img: "music-26",
+    src: "music-26"
+  }, 
+  {
+    name: "Lilac", 
+    artist: "IU",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "Home Sweet Home", 
+    artist: "Car The Garden",
+    img: "music-28",
+    src: "music-28"
+  }, 
+  {
+    name: "For The hesitant lover", 
+    artist: "JANNABI",
+    img: "music-29",
+    src: "music-29"
+  } 
+ 
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
